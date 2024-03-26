8326ee9e3ebcc6e0d21fc6a85d2afa5a5b5cc52f
Raúl Villares <raul@devengo.com>
2024-03-26 19:41:55 +0100

    Add items controller specs

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,44 +1,51 @@
 class ItemsController < ApplicationController
+  before_action :set_list
+  before_action :set_item, only: [:edit, :update, :destroy]
+
   def new
-    @list = List.find(params[:list_id])
     @item = @list.items.build
   end
 
   def create
-    @list = List.find(params[:list_id])
     @item = @list.items.build(item_params)
 
     if @item.save
-      redirect_to @list, notice: 'Item successfully added to the list.'
+      redirect_to @list, notice: "Item successfully added to the list."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  def edit
-    @list = List.find(params[:list_id])
-    @item = @list.items.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @list = List.find(params[:list_id])
-    @item = @list.items.find(params[:id])
-
     if @item.update(item_params)
-      redirect_to list_path(@list), notice: 'Item was successfully updated.'
+      redirect_to list_path(@list), notice: "Item was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @list = List.find(params[:list_id])
-    @item = @list.items.find(params[:id])
     @item.destroy
     redirect_to list_path(@list), notice: "Item was successfully deleted."
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "List not found."
+    redirect_to lists_path
+  end
+
+  def set_item
+    @item = @list.items.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "Item not found."
+    redirect_to list_path(@list)
+  end
 
   def item_params
     params.require(:item).permit(:name)
---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,200 @@
+require 'rails_helper'
+
+RSpec.describe ItemsController, type: :controller do
+  describe "GET new" do
+    it "renders the new item page" do
+      list = List.create(title: "Test List")
+
+      get :new, params: { list_id: list.id }
+
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template(:new)
+      expect(assigns(:list)).to eq(list)
+      expect(assigns(:item)).to be_a_new(Item)
+    end
+  end
+
+  describe "POST create" do
+    context "happy path" do
+      it "creates a new item in the list" do
+        list = List.create(title: "Test List")
+
+        expect {
+          post :create, params: { list_id: list.id, item: { name: "An item name" } }
+        }.to change(Item, :count).by(1)
+
+        expect(response).to redirect_to(list_path(list))
+        expect(flash[:notice]).to eq("Item successfully added to the list.")
+      end
+    end
+
+    context "with invalid item name" do
+      it "does not create the index and renders the new template" do
+        list = List.create(title: "Test List")
+
+        expect {
+          post :create, params: { list_id:  list.id, item: { name: "" } }
+        }.not_to change(Item, :count)
+
+        expect(response).to render_template(:new)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "with duplicated item name" do
+      it "does not create the index and renders the new template" do
+        list = List.create(title: "Test List")
+        existing_item = Item.create(name: "Item name", list: list)
+
+        expect {
+          post :create, params: { list_id:  list.id, item: { name: "Item name" } }
+        }.not_to change(Item, :count)
+
+        expect(response).to render_template(:new)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "when list does not exist" do
+      it "redirects to the lists index page with a not found message" do
+        expect {
+          post :create, params: { list_id: "non_existing_list_id", item: { name: "An item name" } }
+        }.not_to change(Item, :count)
+
+        expect(response).to redirect_to(lists_path)
+        expect(flash[:alert]).to eq("List not found.")
+      end
+    end
+  end
+
+  describe "GET edit" do
+    context "happy path" do
+      it "renders the edit item page" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        get :edit, params: { list_id: list.id, id: item.id }
+
+        expect(response).to have_http_status(:ok)
+        expect(response).to render_template(:edit)
+        expect(assigns(:list)).to eq(list)
+        expect(assigns(:item)).to eq(item)
+      end
+    end
+
+    context "when list does not exist" do
+      it "redirects to the lists index page with a not found message" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        get :edit, params: { list_id: "non_existing_list_id", id: item.id }
+
+        expect(response).to redirect_to(lists_path)
+        expect(flash[:alert]).to eq("List not found.")
+      end
+    end
+
+    context "when item does not exist" do
+      it "redirects to the list page with a not found message" do
+        list = List.create(title: "Test List")
+
+        get :edit, params: { list_id: list.id, id: "non_existing_item_id" }
+
+        expect(response).to redirect_to(list_path(list.id))
+        expect(flash[:alert]).to eq("Item not found.")
+      end
+    end
+  end
+
+  describe "PUT update" do
+    context "happy path" do
+      it "updates item and redirects to list page with successful message" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        put :update, params: { list_id: list.id, id: item.id, item: {name: "New item name"}}
+
+        item.reload
+        expect(item.name).to eq "New item name"
+        expect(response).to redirect_to(list_path(list.id))
+        expect(flash[:notice]).to eq("Item was successfully updated.")
+      end
+    end
+
+    context "when item can't be updated" do
+      it "does not update the item and renders the edit item page" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        put :update, params: { list_id: list.id, id: item.id, item: {name: ""}}
+
+        item.reload
+        expect(item.name).to eq "Item name"
+        expect(response).to render_template(:edit)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+
+    context "when item does not exist" do
+      it "redirects to the list page with a not found message" do
+        list = List.create(title: "Test List")
+
+        put :update, params: { list_id: list.id, id: "non_existing_item_id" }
+
+        expect(response).to redirect_to(list_path(list.id))
+        expect(flash[:alert]).to eq("Item not found.")
+      end
+    end
+
+    context "when list does not exist" do
+      it "redirects to the lists index page with a not found message" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        put :update, params: { list_id: "non_existing_list_id", id: item.id }
+
+        expect(response).to redirect_to(lists_path)
+        expect(flash[:alert]).to eq("List not found.")
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    context "happy path" do
+      it "deletes the item and redirects to list page with successful message" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        expect {
+          delete :destroy, params: { list_id: list.id, id: item.id }
+        }.to change(Item, :count).by(-1)
+
+        expect(response).to redirect_to(list_path(list.id))
+        expect(flash[:notice]).to eq("Item was successfully deleted.")
+      end
+    end
+
+    context "when item does not exist" do
+      it "redirects to the list page with a not found message" do
+        list = List.create(title: "Test List")
+
+        delete :destroy, params: { list_id: list.id, id: "non_existing_item_id" }
+
+        expect(response).to redirect_to(list_path(list.id))
+        expect(flash[:alert]).to eq("Item not found.")
+      end
+    end
+
+    context "when list does not exist" do
+      it "redirects to the lists index page with a not found message" do
+        list = List.create(title: "Test List")
+        item = Item.create(name: "Item name", list: list)
+
+        delete :destroy, params: { list_id: "non_existing_list_id", id: item.id }
+
+        expect(response).to redirect_to(lists_path)
+        expect(flash[:alert]).to eq("List not found.")
+      end
+    end
+  end
+end
